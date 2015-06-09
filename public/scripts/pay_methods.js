b2b90b0e253fa96bd6ace14d46c1b7e3bf150d1e
vitaliy <vital.gm.dev@gmail.com>
2015-06-09 17:41:47 +0300

    Paypal update: fix form submit for Firefox

--- public/scripts/pay_methods.js
+++ public/scripts/pay_methods.js
@@ -4,23 +4,11 @@
 
 $(document).ready(function() {
     $('a.paypal').click(function(e){
-        var custom = {};
-        custom.user = $.cookie("userId");
         if($('input.amount').val() > 0) {
-            console.log('DATA : ', $('input.amount').val(), JSON.stringify(custom));
-            $('<form method="post" action="https://www.sandbox.paypal.com/cgi-bin/webscr" class="paypal-button" target="_top">' +
-            '<div class="hide" id="errorBox"></div>' +
-            '<input type="hidden" name="button" value="buynow">' +
-            '<input type="hidden" name="item_name" value="Addition">' +
-            '<input type="hidden" name="quantity" value="1">' +
-            '<input type="hidden" name="amount" value="' + $('input.amount').val() + '">' +
-            '<input type="hidden" name="custom" value="' + JSON.stringify(custom) + '">' +
-            '<input type="hidden" name="currency_code" value="USD">' +
-            '<input type="hidden" name="env" value="www.sandbox">' +
-            '<input type="hidden" name="cmd" value="_xclick">' +
-            '<input type="hidden" name="business" value="TENPTSTHJ3KVS">' +
-            '<input type="hidden" name="bn" value="JavaScriptButton_buynow">' +
-            '</form>').submit();
+            console.log('DATA', $("input[name='amount']"), $('input.amount').val(), $.cookie("userId"));
+            $("input[name='amount']").val($('input.amount').val());
+            $("input[name='custom']").val(JSON.stringify({userId: $.cookie("userId")}));
+            $("form.paypal-button").submit();
         }
     });
 });
