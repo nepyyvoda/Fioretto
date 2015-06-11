6c04243b6b2343260e88f419b77673263debcc5b
vitaliy <vital.gm.dev@gmail.com>
2015-06-11 11:09:00 +0300

    Add payments user

--- controllers/paypal.js
+++ controllers/paypal.js
@@ -5,6 +5,7 @@
 var querystring = require('querystring');
 var request = require('request');
 var Payment = require('../models/paypalpay');
+var UsersPayments = require('../models/userspayments');
 var log = require('../logger')(module);
 var UserModel = require('../models/user');
 
@@ -22,102 +23,129 @@ function ipn_processor(req, res){
 
     Payment.create(user_data_from_ipn.userId, data_body['mc_gross'],new Date(data_body['payment_date']).getTime(),data_body['ipn_track_id'], data_body['txn_id'], function(status, id){
         // read the IPN message sent from PayPal and prepend 'cmd=_notify-validate'
-        var postreq = 'cmd=_notify-validate';
-        log.info("ID = ", id);
-        for (var key in req.body) {
-            if (req.body.hasOwnProperty(key)) {
-                var value = querystring.escape(req.body[key]);
-                postreq = postreq + "&" + key + "=" + value;
-            }
-        }
-
-        // Step 2: POST IPN data back to PayPal to validate
-        log.info('Posting back to paypal');
-        log.info(postreq);
-        log.info('\n\n');
-        var options = {
-            url: 'https://www.sandbox.paypal.com/cgi-bin/webscr',
-            method: 'POST',
-            headers: {
-                'Connection': 'close'
-            },
-            body: postreq,
-            strictSSL: true,
-            rejectUnauthorized: false,
-            requestCert: true,
-            agent: false
-        };
-
-        request(options, function callback(error, response, body) {
-            if (!error && response.statusCode === 200) {
-
-                // inspect IPN validation result and act accordingly
-
-                if (body.substring(0, 8) === 'VERIFIED'){
-                    // The IPN is verified, process it
-                    log.info('Verified IPN!');
-                    log.info('\n\n');
-
-                    // assign posted variables to local variables
-                    var item_name = req.body['item_name'];
-                    var item_number = req.body['item_number'];
-                    var payment_status = req.body['payment_status'];
-                    var payment_amount = req.body['mc_gross'];
-                    var payment_currency = req.body['mc_currency'];
-                    var txn_id = req.body['txn_id'];
-                    var receiver_email = req.body['receiver_email'];
-                    var payer_email = req.body['payer_email'];
-
-                    //Lets check a variable
-                    log.info("Checking variable");
-                    log.info("payment_status:", payment_status)
-                    log.info('\n\n');
-
-                    // IPN message values depend upon the type of notification sent.
-                    // To loop through the &_POST array and print the NV pairs to the screen:
-                    log.info('Printing all key-value pairs...')
+        if(!status){
+            UsersPayments.create(user_data_from_ipn.userId,
+                data_body['payer_id'],
+                data_body['receiver_id'],
+                0,
+                0,
+                new Date(data_body['payment_date']),
+                0,
+                0,
+                0,
+                'start',
+                data_body['txn_id'],
+                0,
+                data_body['mc_gross'],
+                0,
+                0,
+                0x0,function(status, userpaymentid){
+                    var postreq = 'cmd=_notify-validate';
+                    log.info("ID = ", id);
                     for (var key in req.body) {
                         if (req.body.hasOwnProperty(key)) {
-                            var value = req.body[key];
-                            log.info(key + "=" + value);
+                            var value = querystring.escape(req.body[key]);
+                            postreq = postreq + "&" + key + "=" + value;
                         }
                     }
 
-                    Payment.update(id, {status:"complete"}, function(status, data){
-                        if (!status) {
-                            log.info('Success Complete Paypal pay update', data);
-                            UserModel.get(user_data_from_ipn.userId, function(statusErr, data){
-                                if(!statusErr){
-                                    log.info("USERMODEL :", data);
-                                    var newBalance = +data[0].balance + +payment_amount;
-                                    newBalance * 100;
-                                    log.info("USERMODEL :", user_data_from_ipn.userId, req.body);
-                                    UserModel.update(user_data_from_ipn.userId, {balance: newBalance}, function(statusErr, data){
-                                        if(!statusErr){
-                                            log.info("USER UPDATE SUCCESS! ", data);
-                                        }else{
-                                            log.warn("USER NOT UPDATE SUCCESS! ", data);
-                                        }
-                                    });
-                                }
-                            });
-
-                        }else
-                            log.info(data);
-                    });
-
-                } else if (body.substring(0, 7) === 'INVALID') {
-                    // IPN invalid, log for manual investigation
-                    log.info('Invalid IPN!');
+                    // Step 2: POST IPN data back to PayPal to validate
+                    log.info('Posting back to paypal');
+                    log.info(postreq);
                     log.info('\n\n');
-                    Payment.update(id, {status:"error"}, function(status, data){
-                        if (status)
-                            log.info(data);
-                    });
-                }
+                    var options = {
+                        url: 'https://www.sandbox.paypal.com/cgi-bin/webscr',
+                        method: 'POST',
+                        headers: {
+                            'Connection': 'close'
+                        },
+                        body: postreq,
+                        strictSSL: true,
+                        rejectUnauthorized: false,
+                        requestCert: true,
+                        agent: false
+                    };
 
-            }
-        });
+                    request(options, function callback(error, response, body) {
+                        if (!error && response.statusCode === 200) {
+
+                            // inspect IPN validation result and act accordingly
+
+                            if (body.substring(0, 8) === 'VERIFIED'){
+                                // The IPN is verified, process it
+                                log.info('Verified IPN!');
+                                log.info('\n\n');
+
+                                // assign posted variables to local variables
+                                var item_name = req.body['item_name'];
+                                var item_number = req.body['item_number'];
+                                var payment_status = req.body['payment_status'];
+                                var payment_amount = req.body['mc_gross'];
+                                var payment_currency = req.body['mc_currency'];
+                                var txn_id = req.body['txn_id'];
+                                var receiver_email = req.body['receiver_email'];
+                                var payer_email = req.body['payer_email'];
+
+                                //Lets check a variable
+                                log.info("Checking variable");
+                                log.info("payment_status:", payment_status)
+                                log.info('\n\n');
+
+                                // IPN message values depend upon the type of notification sent.
+                                // To loop through the &_POST array and print the NV pairs to the screen:
+                                log.info('Printing all key-value pairs...')
+                                for (var key in req.body) {
+                                    if (req.body.hasOwnProperty(key)) {
+                                        var value = req.body[key];
+                                        log.info(key + "=" + value);
+                                    }
+                                }
+
+                                Payment.update(id, {status:"complete"}, function(status, data){
+                                    if (!status) {
+                                        log.info('Success Complete Paypal pay update', data);
+                                        UserModel.get(user_data_from_ipn.userId, function(statusErr, data){
+                                            if(!statusErr){
+                                                log.info("USERMODEL :", data);
+                                                var newBalance = +data[0].balance + +payment_amount;
+                                                //parse to cents
+                                                newBalance *= 100;
+                                                log.info("USERMODEL :", user_data_from_ipn.userId, req.body);
+                                                UserModel.update(user_data_from_ipn.userId, {balance: newBalance}, function(statusErr, data){
+                                                    if(!statusErr){
+                                                        log.info("USER UPDATE SUCCESS! ", data);
+                                                        UsersPayments.update(userpaymentid, {end_date: new Date(), textStatus: 'complete'}, function(status, data){
+                                                            if(!status){
+                                                                log.info("USERPAYMENTS UPDATE SUCCESS! ", data);
+                                                            } else {
+                                                                log.warn("USERPAYMENTS NOT UPDATE SUCCESS! ", data);
+                                                            }
+                                                        });
+                                                    }else{
+                                                        log.warn("USER NOT UPDATE SUCCESS! ", data);
+                                                    }
+                                                });
+                                            }
+                                        });
+
+                                    }else
+                                        log.info(data);
+                                });
+
+                            } else if (body.substring(0, 7) === 'INVALID') {
+                                // IPN invalid, log for manual investigation
+                                log.info('Invalid IPN!');
+                                log.info('\n\n');
+                                Payment.update(id, {status:"error"}, function(status, data){
+                                    if (status)
+                                        log.info(data);
+                                });
+                            }
+
+                        }
+                    });
+                });
+        }
     });
 }
 
--- models/userspayments.js
+++ models/userspayments.js
@@ -3,22 +3,84 @@
  */
 
 var execute = require('../db').execute;
+var formatter = require('./formatter/update');
 
-function create(login, sourceid, reciverid, servicepaymentid, starttime, transactiontypeid, transactionid, currencyid, amount, payschemid, commission){
-    execute('INSERT INTO userspayments (login, sourceID, receiverID, servicesPaymentID, start_time, transactionTypeID, transactionID, currencyID, amount, paymentSchemeID, commission) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        [login, sourceid, reciverid, servicepaymentid, starttime, transactiontypeid, transactionid, currencyid, amount, payschemid, commission],
+var allowedUpdateColumns = [
+    'end_time',
+    'statusPaymentID',
+    'textStatus',
+    'deleted'
+];
+
+function create(login,
+                sourceID,
+                receiverID,
+                servicesPaymentID,
+                servicesID,
+                start_time,
+                end_time,
+                transactionTypeID,
+                statusPaymentID,
+                textStatus,
+                transactionID,
+                currencyID,
+                amount,
+                paymentSchemeID,
+                commission,
+                deleted, callback){
+    execute('INSERT INTO userspayments (login, ' +
+        'sourceID, ' +
+        'receiverID, ' +
+        'servicesPaymentID, ' +
+        'servicesID,' +
+        'start_time, ' +
+        'end_time,' +
+        'transactionTypeID, ' +
+        'statusPaymentID,' +
+        'textStatus,' +
+        'transactionID, ' +
+        'currencyID, ' +
+        'amount, ' +
+        'paymentSchemeID, ' +
+        'commission, ' +
+        'deleted)' +
+        'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        [login, sourceID, receiverID, servicesPaymentID, servicesID, start_time, end_time, transactionTypeID, statusPaymentID, textStatus, transactionID, currencyID,
+            amount,
+            paymentSchemeID,
+            commission,
+            deleted],
         function(data) {
             callback(false, data);
         }
     );
 }
 
-function update(id, parametr, value, callback){
+/*function update(id, parametr, value, callback){
     execute('UPDATE userspayments SET ?? = ? WHERE `id` = ?', [parametr, value, id], function(err, data){
         if(data.length > 0){
             callback (false, data);
         } else {
             callback (true, 'PAYMENT_NOT_FOUND');
+        }
+    });
+}*/
+
+function update(id, data, callback){
+    var formattedData = formatter(id, data, allowedUpdateColumns);
+    if(!formattedData) {
+        callback (true, 'ILLEGAL_COLUMNS');
+    }
+
+    var queryTemplate = formattedData.template || '';
+    var queryData = formattedData.data || [];
+    //log.info("PARAMS QUERY: " +  queryTemplate + " || " + queryData +"||\n\n");
+    execute('UPDATE userspayments SET ' + queryTemplate + ' WHERE `id` = ?', queryData, function(err, data){
+        log.info("DATA userspayments: ", data);
+        if(data.changedRows > 0){
+            callback (false, data);
+        } else {
+            callback (true, 'USERPAYMENTS_NOT_FOUND');
         }
     });
 }
