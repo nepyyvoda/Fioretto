7f173ae5b8459d18f686ea69694d0debd888d164
vitaliy <vital.gm.dev@gmail.com>
2015-06-11 14:45:38 +0300

    TESTING: ADD LOG for update user

--- models/user.js
+++ models/user.js
@@ -85,7 +85,7 @@ function update(id, data, callback){
     var queryTemplate = formattedData.template || '';
     var queryData = formattedData.data || [];
     execute('UPDATE users SET ' + queryTemplate + ' WHERE `id` = ?', queryData, function(err, data){
-        console.log("USER model UPDATE :", JSON.parse(data));
+        console.log("USER model UPDATE :", data);
         if(data.changedRows > 0){
             callback (false, data);
         } else {
