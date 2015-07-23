6f02f3bb1fa755f260c48916d06e02be43012b43
vitaliy <vital.gm.dev@gmail.com>
2015-07-23 15:55:15 +0300

    UPD: - update loading config

--- config/index.js
+++ config/index.js
@@ -5,33 +5,21 @@ var fs = require('fs');
 var nconf = require('nconf');
 var path = require('path');
 
-fs.open('config.local.json', 'r', function(err, fd){
-    if(err !== null){
-        if(err.code === 'ENOENT'){
-            console.log("Can't open  : config.local.json - not exist", err.code );
-            fs.open('config.json', 'r', function(err, fd){
-                if(err !== null){
-                    if(err.code === 'ENOENT'){
-                        console.log("Can't open  : config.json - not exist", err.code );
-                    } else {
-                        console.log("Can't open  : config.json ", err.code);
-                    }
-                } else {
-                    nconf.argv()
-                        .env()
-                        .file({file: path.join(__dirname, 'config.json')});
-
-                    module.exports = nconf;
-                }
-            });
-        } else {
-            console.log("Can't open  : config.local.json code = ", err.code);
-        }
-    } else {
+try{
+    fs.openSync(path.join(__dirname, 'config.local.json'), 'r');
+    nconf.argv()
+        .env()
+        .file({file: path.join(__dirname, 'config.local.json')});
+} catch (err){
+    try{
+        fs.openSync(path.join(__dirname, 'config.json'), 'r');
         nconf.argv()
             .env()
             .file({file: path.join(__dirname, 'config.json')});
-
-        module.exports = nconf;
+    } catch (err){
+        console.log(err);
     }
-});
+    console.log(err);
+}
+
+module.exports = nconf;
