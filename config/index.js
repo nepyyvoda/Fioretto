c91a3d887036f2ffa45e603aa776e6ad74cb97ff
vitaliy <vital.gm.dev@gmail.com>
2015-07-23 15:32:37 +0300

    UPD: - config add to gitignore

--- config/index.js
+++ config/index.js
@@ -1,11 +1,37 @@
 /**
- * Created by anton.nepyyvoda on 03.04.2015.
+ * Created by vitaliy on 23.07.15.
  */
+var fs = require('fs');
 var nconf = require('nconf');
 var path = require('path');
 
-nconf.argv()
-    .env()
-    .file({file: path.join(__dirname, 'config.json')});
+fs.open('config.local.json', 'r', function(err, fd){
+    if(err !== null){
+        if(err.code === 'ENOENT'){
+            console.log("Can't open  : config.local.json - not exist", err.code );
+            fs.open('config.json', 'r', function(err, fd){
+                if(err !== null){
+                    if(err.code === 'ENOENT'){
+                        console.log("Can't open  : config.json - not exist", err.code );
+                    } else {
+                        console.log("Can't open  : config.json ", err.code);
+                    }
+                } else {
+                    nconf.argv()
+                        .env()
+                        .file({file: path.join(__dirname, 'config.json')});
 
-module.exports = nconf;
+                    module.exports = nconf;
+                }
+            });
+        } else {
+            console.log("Can't open  : config.local.json code = ", err.code);
+        }
+    } else {
+        nconf.argv()
+            .env()
+            .file({file: path.join(__dirname, 'config.json')});
+
+        module.exports = nconf;
+    }
+});
