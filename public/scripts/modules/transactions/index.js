626a20077b61b548779f8ae9eeb0c244991e77ec
a.nepyyvoda <a.nepyyvoda@gmail.com>
2015-08-11 10:52:12 +0300

    admin dev

--- public/scripts/modules/transactions/index.js
+++ public/scripts/modules/transactions/index.js
@@ -1,7 +1,38 @@
 /**
  * Created by anton.nepyyvoda on 05.08.2015.
  */
-define(['jquery', 'knockout', './transaction/index'], function($, ko, TransactionModel) {
+define(['jquery', 'knockout', './transaction/index', 'datatables'], function($, ko, TransactionModel, datatables) {
+    //console.log(datatables dataTable);
+    function GetParams() {
+        var self = this;
+        self.params = [];
+        self.push = function(object) {
+            self.params.push(object);
+            console.log(self.params);
+        };
+        self.pop = function() {
+            return self.params.pop();
+        };
+        self.get = function() {
+            if(self.params.length >= 0) {
+                var q = '?';
+                for(var i = 1; i <= self.params.length; i++) {
+                    if(i === self.params.length) {
+                        q += Object.keys(self.params[i-1])[0] + '=' + self.params[i-1][Object.keys(self.params[i-1])[0]];
+                    } else {
+                        q += Object.keys(self.params[i-1])[0] + '=' + self.params[i-1][Object.keys(self.params[i-1])[0]] + '&';
+                    }
+                }
+                return q;
+            } else {
+                return '';
+            }
+        };
+        self.getParam = function(paramName) {
+
+        };
+    }
+
     return function () {
         var self = this;
         self.transactions = ko.observableArray();
@@ -12,11 +43,33 @@ define(['jquery', 'knockout', './transaction/index'], function($, ko, Transactio
         self.isLoading = ko.observable(false);
         self.isError = ko.observable(false);
         self.errorText = ko.observable(false);
-        self.getList = function() {
 
-        };
+        self.limit = ko.observable();
+        self.offset = ko.observable();
+        self.from = ko.observable();
+        self.to = ko.observable();
+
+        self.filterBy = ko.observable();
+        self.filterValue = ko.observable();
+        self.orderBy = ko.observable();
+        self.orderDirection = ko.observable();
+
+        self.searchBy = ko.observable();
+        self.searchValue = ko.observable();
+
         self.search = function() {
 
+            var par = new GetParams();
+            par.push({limit: self.limit()});
+            par.push({offset: self.offset()});
+
+            $.getJSON('/api/admin/payments' + par.get(), function(res) {
+                if(res.status === 0) {
+
+                } else {
+                    console.log('Error');
+                }
+            });
         };
         self.getAllTransactions = function() {
             $.getJSON('/api/admin/payments', function(res) {
--- public/scripts/modules/transactions/transaction/index.js
+++ public/scripts/modules/transactions/transaction/index.js
@@ -24,10 +24,35 @@ define(['jquery', 'knockout'], function($, ko) {
         self.serviceId = ko.observable(data.servicesID);
         self.amount = ko.observable(data.amount);
         self.currency = ko.observable(data.currencyID);
+        self.currencyPrintable = ko.computed(function() {
+            var currencyName = 'USD';
+            switch(self.currency()) {
+                case 1:
+                    currencyName = 'USD';
+                    break;
+                case 2:
+                    currencyName = 'EURO';
+                    break;
+                case 3:
+                    currencyName = 'UAH';
+                    break;
+                case 4:
+                    currencyName = 'RUB';
+                    break;
+            }
+            return currencyName;
+        });
+
         self.type = ko.observable(data.transactionTypeID);
+        self.typePrintable = ko.computed(function() {
+
+        });
         self.transactionId = ko.observable(data.transactionID);
         self.commission = ko.observable(data.commission);
         self.performDate = ko.observable(data.end_time);
+        self.performDatePrintable = ko.computed(function() {
+
+        });
         self.userId = ko.observable(data.login);
         self.status = ko.observable(data.textStatus);
     };
